42a3d5f7b20b034a68b6004296a9f47aa6346de0
DillonW55 <112511507+DillonW55@users.noreply.github.com>
2022-12-01 14:18:17 -0600

    Day 1

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,7 +1,35 @@
-public class Day01 {
+import java.util.Scanner;
+import java.io.File;
 
+public class Day01 {
+    private final static String DATA_FILE = "input.txt";
     public static void part1() {
 
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch(Exception e) {
+            System.out.println(e);
+            return;
+        }
+        int mostCalories = 0;
+        
+        while(in.hasNextLine()) {
+            String line = in.nextLine();
+            int calories = 0;
+            while(!line.equals("")) {
+                calories += Integer.parseInt(line);
+                if(in.hasNext())
+                    line = in.nextLine();
+                else
+                    break;
+            }
+            if(calories> mostCalories){
+                mostCalories = calories;
+            }
+
+        }
+        System.out.println(mostCalories);
     }
 
 
@@ -10,5 +38,7 @@ public class Day01 {
     }
 
 
-
+    public static void main(String[] args) {
+        part1();
+    }
 }
