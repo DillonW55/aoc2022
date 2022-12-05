60488dc2a408523427b50ac3a5150fe9254c9bbc
DillonW55 <112511507+DillonW55@users.noreply.github.com>
2022-12-05 14:16:28 -0600

    Day2

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,71 +1,77 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.io.File;
 
 public class Day01 {
-    private final static String DATA_FILE = "input.txt";
-    public static void part1() {
 
+    private final static String DATA_FILE = "input.txt";
+
+    public static void part1() {
         Scanner in;
         try {
             in = new Scanner(new File(DATA_FILE));
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
             return;
         }
         int mostCalories = 0;
-        
-        while(in.hasNextLine()) {
+
+        while (in.hasNextLine()) {
             String line = in.nextLine();
             int calories = 0;
-            while(!line.equals("")) {
+
+            // get total calories not separated by a space
+            while (!line.equals("") && in.hasNext()) {
                 calories += Integer.parseInt(line);
-                if(in.hasNext())
-                    line = in.nextLine();
-                else
-                    break;
+                line = in.nextLine();
             }
-            if(calories> mostCalories){
+
+            if (calories > mostCalories) {
                 mostCalories = calories;
             }
-
         }
-        System.out.println(mostCalories);
-    }
 
-    private final static String DATA_FILE = "input.txt";
+        System.out.println(mostCalories);
+
+    }
 
     public static void part2() {
         Scanner in;
         try {
             in = new Scanner(new File(DATA_FILE));
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
             return;
         }
-        ArrayList<Integer;
-        
-        while(in.hasNextLine()) {
+        ArrayList<Integer> calorieList = new ArrayList<Integer>();
+
+        while (in.hasNext()) {
             String line = in.nextLine();
             int calories = 0;
-            while(!line.equals("")) {
+
+            // get total calories not separated by a space
+            while (!line.equals("")) {
                 calories += Integer.parseInt(line);
-                if(in.hasNext())
+                if (in.hasNext()) {
                     line = in.nextLine();
-                else
+                } else {
                     break;
+                }
             }
-            if(calories> mostCalories){
-                mostCalories = calories;
-            }
+
+            calorieList.add(calories);
 
         }
-        System.out.println(mostCalories);
-    }
+        Collections.sort(calorieList);
+        Collections.reverse(calorieList);
+        // System.out.println(calorieList);
+        System.out.println(calorieList.remove(0) + calorieList.remove(0) + calorieList.remove(0));
 
     }
-
 
     public static void main(String[] args) {
         part2();
     }
+
 }
